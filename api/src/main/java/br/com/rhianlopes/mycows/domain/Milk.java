24302172bfac02593fa99b5505c07dac7c1f4e96
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-03-26 21:18:37 -0300

    feat(Domain): mapping last entity with ddl initial data

---
+++ api/src/main/java/br/com/rhianlopes/mycows/domain/Milk.java
@@ -0,0 +1,40 @@
+package br.com.rhianlopes.mycows.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.istack.NotNull;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Accessors(chain = true)
+public class Milk {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cow_id")
+    private Cow cow;
+
+    @NotNull
+    private Double litters;
+
+    @NotNull
+    @Size(max = 200)
+    private String description;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime createdAt;
+
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime updatedAt;
+}
