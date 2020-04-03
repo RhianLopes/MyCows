90ec3e9e9a46b39e38609b84ee1d4acb8365c6d0
rhian.costa@cwi.com.br <rhianlopes63@gmail.com>
2020-04-02 21:05:53 -0300

    feat(ConfigSwagger): add the configs to able swagger

--- api/src/main/java/br/com/rhianlopes/mycows/controller/security/AuthenticationController.java
+++ api/src/main/java/br/com/rhianlopes/mycows/controller/security/AuthenticationController.java
@@ -3,6 +3,8 @@ package br.com.rhianlopes.mycows.controller.security;
 import br.com.rhianlopes.mycows.controller.security.request.LoginRequestDto;
 import br.com.rhianlopes.mycows.controller.security.response.LoginResponseDto;
 import br.com.rhianlopes.mycows.service.security.AuthenticationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import javax.validation.Valid;
  * @author rhian.costa
  */
 @RestController
+@Api(tags = "Auth")
 @RequiredArgsConstructor
 @RequestMapping("/public/auth")
 public class AuthenticationController {
@@ -23,6 +26,7 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     @ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Login a User with E-mail and Password")
     public LoginResponseDto login(@RequestBody @Valid LoginRequestDto request) {
         return new LoginResponseDto(service.authenticate(request.getEmail(), request.getPassword()));
     }
