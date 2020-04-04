8cbd76e0b02e531a799f919a82c5e925d3ca37c8
rhian.costa@cwi.com.br <rhianlopes63@gmail.com>
2020-04-03 21:43:04 -0300

    feat(CrudUser): add implementation with bug to find logged user

--- api/src/main/java/br/com/rhianlopes/mycows/controller/find/FindController.java
+++ api/src/main/java/br/com/rhianlopes/mycows/controller/find/FindController.java
@@ -9,27 +9,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
 import javax.validation.constraints.Email;
 
 /**
  * @author rhian.costa
  */
 @RestController
-@Api(tags = { "Find One", "Find All" })
+@Api(tags = { "Find One" })
 @RequiredArgsConstructor
 @RequestMapping("/find")
 public class FindController {
 
     private final UserService userService;
 
+    @RolesAllowed({ "ROLE_USER" })
     @GetMapping("/logged-user")
-    @ApiOperation(value = "Find Logged User", tags = "Find One")
+    @ApiOperation(value = "Find Logged User")
     public User findOneUser(@AuthenticationPrincipal UserPrincipal userPrincipal) {
         return userService.findById(userPrincipal);
     }
 
+    @RolesAllowed({ "ROLE_USER" })
     @GetMapping("/user")
-    @ApiOperation(value = "Find User By Email", tags = "Find One")
+    @ApiOperation(value = "Find User By Email")
     public User findByEmail(@RequestParam(name = "email") @Email String email) {
         return userService.findByEmail(email);
     }
