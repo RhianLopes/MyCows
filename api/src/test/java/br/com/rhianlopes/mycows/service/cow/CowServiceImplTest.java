737dbf3e90ffe4eceedaa02e1ae303bc2a4a86bf
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-04-20 21:51:27 -0300

    feat(CowServiceTest): created the initial test to cow service

---
+++ api/src/test/java/br/com/rhianlopes/mycows/service/cow/CowServiceImplTest.java
@@ -0,0 +1,30 @@
+package br.com.rhianlopes.mycows.service.cow;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rhian.costa
+ */
+@ExtendWith(MockitoExtension.class)
+public class CowServiceImplTest {
+
+    @Test
+    public void registerCow() {
+    }
+
+    @Test
+    public void editCow() {
+    }
+
+    @Test
+    public void findCowByIdAndUserId() {
+    }
+
+    @Test
+    public void findAllByFarmIdAndUserIdAndIsActive() {
+    }
+}
