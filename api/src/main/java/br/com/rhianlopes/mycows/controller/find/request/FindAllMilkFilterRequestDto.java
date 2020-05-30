f73e54cd7525c91824ff4428787956a0e8974613
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-05-30 17:22:57 -0300

    fix: add some ajusts to func the service

--- api/src/main/java/br/com/rhianlopes/mycows/controller/find/request/FindAllMilkFilterRequestDto.java
+++ api/src/main/java/br/com/rhianlopes/mycows/controller/find/request/FindAllMilkFilterRequestDto.java
@@ -1,9 +1,11 @@
 package br.com.rhianlopes.mycows.controller.find.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
@@ -20,7 +22,11 @@ public class FindAllMilkFilterRequestDto {
     @NotNull
     private Long cowId;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate initialDate;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate finalDate;
 }
